f829dd7156d2435deadfa7cd68f234e0df38cee9
Hoanpx <hp.831557@pcpeco.com>
2023-11-01 08:57:24 +0700

    [add func] handle received data ,save to DB and show it

--- logrecord.cpp
+++ logrecord.cpp
@@ -1,11 +1,132 @@
 #include "logrecord.h"
+#include <QDebug>
+#include <QDataStream>
 
 LogRecord::LogRecord()
 {
-
+    setSequent(23);
 }
 
 QString LogRecord::getNameTable()
 {
     return nameTable;
+}
+
+int LogRecord::getSeqence()
+{
+    return sequence;
+}
+
+char LogRecord::getId()
+{
+    return id;
+}
+
+char LogRecord::getStatus()
+{
+    return status;
+}
+
+char* LogRecord::getLiter_1()
+{
+    return liter_1;
+}
+
+char* LogRecord::getUnitPrice_1()
+{
+    return unitPrice_1;
+}
+
+char* LogRecord::getMoney_1()
+{
+    return money_1;
+}
+
+char* LogRecord::getLiter_2()
+{
+    return liter_2;
+}
+
+char* LogRecord::getUnitPrice_2()
+{
+    return unitPrice_2;
+}
+
+char* LogRecord::getMoney_2()
+{
+    return money_2;
+}
+
+char* LogRecord::getLiter_3()
+{
+    return liter_3;
+}
+
+char* LogRecord::getUnitPrice_3()
+{
+    return unitPrice_3;
+}
+
+char* LogRecord::getMoney_3()
+{
+    return money_3;
+}
+
+char* LogRecord::getLiter_4()
+{
+    return liter_4;
+}
+
+char* LogRecord::getUnitPrice_4()
+{
+    return unitPrice_4;
+}
+
+char* LogRecord::getMoney_4()
+{
+    return money_4;
+}
+
+char LogRecord::getChecksum()
+{
+    return 1;
+}
+
+void LogRecord::setSequent(int data)
+{
+    sequence = data;
+}
+
+void LogRecord::parseData(QByteArray * const p_data)
+{
+#if 1
+    char *l_data = p_data->data();
+    checksum = l_data[91];
+    if(1){ //checksum == calcChecksum(l_data)
+        sequence = l_data[0];
+        id = l_data[1];
+        status = l_data[2];
+        strncpy((char *)liter_1,(const char *)&l_data[3],8);
+        strncpy((char *)unitPrice_1,(const char *)&l_data[11],6);
+        strncpy((char *)money_1,(const char *)&l_data[17],8);
+        strncpy((char *)liter_2,(const char *)&l_data[25],8);
+        strncpy((char *)unitPrice_2,(const char *)&l_data[33],6);
+        strncpy((char *)money_2,(const char *)&l_data[39],8);
+        strncpy((char *)liter_3,(const char *)&l_data[47],8);
+        strncpy((char *)unitPrice_3,(const char *)&l_data[55],6);
+        strncpy((char *)money_3,(const char *)&l_data[61],8);
+        strncpy((char *)liter_4,(const char *)&l_data[69],8);
+        strncpy((char *)unitPrice_4,(const char *)&l_data[77],6);
+        strncpy((char *)money_4,(const char *)&l_data[83],8);
+    }else{
+        qDebug() << "checksum error";
+    }
+#endif
+}
+
+char LogRecord::calcChecksum(char *data)
+{
+    char l_result;
+    l_result = data[91];
+    return l_result;
 }
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,6 +7,7 @@
 #include <QSqlTableModel>
 #include <QTimer>
 
+static QString mergeCharArray(char* arr,int size);
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -38,7 +39,7 @@ void MainWindow::on_pushOpen_pressed()
         QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
         db.setDatabaseName("mydatabase.db");
         if (!db.open()) {
-            qDebug() << "Cannot careate new database. Lỗi:" << db.lastError().text();
+            qDebug() << "Cannot careate new database. Error:" << db.lastError().text();
         } else {
             qDebug() << QObject::tr("Cơ sở dữ liệu đã được mở!");
             QSqlQuery query;
@@ -69,12 +70,127 @@ void MainWindow::on_pushOpen_pressed()
         //*//
     }
 }
+//sequence,id,status,1,2,3,4
 void MainWindow::readDataSerial(QByteArray data)
 {
     ui->listMessage->addItem(QString (data));
-    QString qry_cmd;
+    QString qry_cmd = "insert into LogRS232 values(";
     QSqlQuery query;
-    qry_cmd.append("insert into LogRS232 values(105,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5)");
+    record.parseData(&data);
+    QString l_data = (data.toHex());
+    QString data_part = l_data.mid(0,2);
+    qry_cmd.append(data_part); //Sequence
+    qry_cmd.append(",");
+    data_part = l_data.mid(2,2);
+    qry_cmd.append(data_part); //ID
+    qry_cmd.append(",");
+    data_part = l_data.mid(4,2);
+    qry_cmd.append(data_part);//Status
+    qry_cmd.append(",");
+    data_part = l_data.mid(6,16);
+    qry_cmd.append(data_part);
+    qry_cmd.append(",");
+    data_part = l_data.mid(22,12);
+    qry_cmd.append(data_part);
+    qry_cmd.append(",");
+    data_part = l_data.mid(34,16);
+    qry_cmd.append(data_part);
+    qry_cmd.append(",");
+    data_part = l_data.mid(50,16);
+    qry_cmd.append(data_part);
+    qry_cmd.append(",");
+    data_part = l_data.mid(66,12);
+    qry_cmd.append(data_part);
+    qry_cmd.append(",");
+    data_part = l_data.mid(78,16);
+    qry_cmd.append(data_part);
+    qry_cmd.append(",");
+    data_part = l_data.mid(94,16);
+    qry_cmd.append(data_part);
+    qry_cmd.append(",");
+    data_part = l_data.mid(110,12);
+    qry_cmd.append(data_part);
+    qry_cmd.append(",");
+    data_part = l_data.mid(122,16);
+    qry_cmd.append(data_part);
+    qry_cmd.append(",");
+    data_part = l_data.mid(138,16);
+    qry_cmd.append(data_part);
+    qry_cmd.append(",");
+    data_part = l_data.mid(154,12);
+    qry_cmd.append(data_part);
+    qry_cmd.append(",");
+    data_part = l_data.mid(166,16);
+    qry_cmd.append(data_part);
+    qry_cmd.append(",");
+//    qry_cmd.append("insert into LogRS232 values(105,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5)");
+#if 0
+    qry_cmd.append(QByteArray::number(record.getSeqence()));
+    qry_cmd.append(",");
+    qry_cmd.append(QByteArray::number(record.getId()));
+    qry_cmd.append(",");
+    qry_cmd.append(QByteArray::number(record.getStatus()));
+    qry_cmd.append(",");
+    QString str;
+    str= mergeCharArray(record.getLiter_1(),8) ;
+    qry_cmd.append((str));
+    qry_cmd.append(",");
+    str = mergeCharArray(record.getUnitPrice_1(),6);
+    qry_cmd.append((str));
+    qry_cmd.append(",");
+    str = mergeCharArray(record.getMoney_1(),8);
+    qry_cmd.append((str));
+    qry_cmd.append(",");
+    str= mergeCharArray(record.getLiter_2(),8) ;
+    qry_cmd.append((str));
+    qry_cmd.append(",");
+    str = mergeCharArray(record.getUnitPrice_2(),6);
+    qry_cmd.append((str));
+    qry_cmd.append(",");
+    str = mergeCharArray(record.getMoney_2(),8);
+    qry_cmd.append((str));
+    qry_cmd.append(",");
+    str= mergeCharArray(record.getLiter_3(),8) ;
+    qry_cmd.append((str));
+    qry_cmd.append(",");
+    str = mergeCharArray(record.getUnitPrice_3(),6);
+    qry_cmd.append((str));
+    qry_cmd.append(",");
+    str = mergeCharArray(record.getMoney_3(),8);
+    qry_cmd.append((str));
+    qry_cmd.append(",");
+    str= mergeCharArray(record.getLiter_4(),8) ;
+    qry_cmd.append((str));
+    qry_cmd.append(",");
+    str = mergeCharArray(record.getUnitPrice_4(),6);
+    qry_cmd.append((str));
+    qry_cmd.append(",");
+    str = mergeCharArray(record.getMoney_4(),8);
+    qry_cmd.append((str));
+    qry_cmd.append(",");
+#endif
+//    qry_cmd.append(QByteArray::number(record.getUnitPrice_1()));
+//    qry_cmd.append(",");
+//    qry_cmd.append(QByteArray::number(record.getMoney_1()));
+//    qry_cmd.append(",");
+//    qry_cmd.append(QByteArray::number(record.getLiter_2()));
+//    qry_cmd.append(",");
+//    qry_cmd.append(QByteArray::number(record.getUnitPrice_2()));
+//    qry_cmd.append(",");
+//    qry_cmd.append(QByteArray::number(record.getMoney_2()));
+//    qry_cmd.append(",");
+//    qry_cmd.append(QByteArray::number(record.getLiter_3()));
+//    qry_cmd.append(",");
+//    qry_cmd.append(QByteArray::number(record.getUnitPrice_3()));
+//    qry_cmd.append(",");
+//    qry_cmd.append(QByteArray::number(record.getMoney_3()));
+//    qry_cmd.append(",");
+//    qry_cmd.append(QByteArray::number(record.getLiter_3()));
+//    qry_cmd.append(",");
+//    qry_cmd.append(QByteArray::number(record.getUnitPrice_4()));
+//    qry_cmd.append(",");
+//    qry_cmd.append(QByteArray::number(record.getMoney_4()));
+    qry_cmd.append("99)");
     query.exec(qry_cmd);
     static QSqlTableModel *model_1 = new QSqlTableModel;
     model_1->setTable("LogRS232");
@@ -90,4 +206,11 @@ void MainWindow::on_btnSend_clicked()
         QMessageBox::critical(this,"Error","Something went wrong!!");
     }
 }
+static QString mergeCharArray(char* arr,int size){
+    QString mergedString;
 
+    for (int i = 0; i < size; ++i) {
+        mergedString.append(QString(arr[i]));
+    }
+    return mergedString;
+}
